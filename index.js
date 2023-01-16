a0dd80f14df75984a48454ed14825647a9766778
Numonu <juan.villegas.dev@gmail.com>
2023-01-16 13:52:32 -0500

    Correccion de errores

--- index.js
+++ index.js
@@ -23,57 +23,75 @@ const claves = {
     ober: "o",
     ufat: "u"
 }
-//Quitar las ilustraciones y mostrar el resultado
+//Quitar las ilustraciones para mostrar el resultado
 function showResult() {
     copyButton.classList.remove("reading__copy-button--disable");
-    help.classList.add("reading__info--disable");
     resultInput.classList.remove("reading__read-space--disable");
+
+    help.classList.add("reading__info--disable");
 }
-//Verifica algunas reglas textuales
+
+//Retornamos un boleano para verificar reglas en el texto
 function verifify(userText) {
-    //solo minusculas y espacios
-    if (/[^a-z\s]/g.test(userText)) 
+    //Verificamos que solo haya caracteres en minuscula
+    if (/[^a-z\s]/g.test(userText))
         alert("Por favor no utilice mayusculas ni caracteres especiales!");
+    //Verificamos que el input no este vacio
     else if (userText.length == 0 || /^[\s]*$/g.test(userText)) {
         userInput.value = "";
         alert("Por favor introduce texto!");
     }
+    //true => Todas las reglas se cumplieron
     else return true;
     return false;
 }
+
 //Funciones principales
 const encriptar = function () {
     const userText = userInput.value;
-    const regex = /a|e|i|o|u/g;
+    const regexRule = /a|e|i|o|u/g;
+    const regexMinLength = /\s?[a-z]{15,}\s?/;
+
     if (verifify(userText)) {
         //Para continuar las palabras deben tener un largo menor a 15
-        if (/\s?[a-z]{15,}\s?/.test(userText)) 
+        if (regexMinLength.test(userText)){
             alert("Por favor no utilice palabras demaciado largas (max:15)");
+        }
+        //Al cumplirse la regla, reemplazamos las vocales por las claves
         else {
-            //Al cumplirse las reglas, mostramos el resultado
-            resultInput.textContent = userText.replace(regex, param => vocales[param]);
+            resultInput.textContent = userText.replace(regexRule, param => vocales[param]);
+            //Mostramos el resultado en el read-space
             showResult();
         }
     }
 }
+
 const desencriptar = function () {
     const userText = userInput.value;
-    const regex = /enter|imes|ai|ober|ufat/g;
+    const regexRule = /enter|imes|ai|ober|ufat/g;
+
     if (verifify(userText)) {
         //Para continuar el texto debe estar encriptado
-        if(regex.test(userText)){
-            resultInput.textContent = userText.replace(regex, param => claves[param]);
+        if (regexRule.test(userText)) {
+            resultInput.textContent = userText.replace(regexRule, param => claves[param]);
+            //Mostramos el resultado en el read-space
             showResult();
         }
         else
             alert("No puedes desencriptar texto no encriptado :P");
     }
 }
-copyButton.addEventListener("click", async () => {
-    await navigator.clipboard.writeText(resultInput.textContent);
-    alert("Texto copiado al portapapeles");
-})
+
 //Eventos
+copyButton.addEventListener("click", async () => {
+    try {
+        await navigator.clipboard.writeText(resultInput.textContent);
+        alert("Texto copiado al portapapeles");
+    }
+    catch{
+        alert("La funcion de copiar no esta disponible");
+    }
+})
 encriptarButton.addEventListener("click", encriptar);
 desencriptarButton.addEventListener("click", desencriptar);
 
